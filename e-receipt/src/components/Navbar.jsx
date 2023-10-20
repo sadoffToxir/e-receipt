9d1b49df728a6caab7502df15169948416aaa21a
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-10-20 19:37:05 +0200

    big push

---
+++ e-receipt/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="menu">Menu</div>
+      <div className="logout">Log Out</div>
+    </div>
+  )
+}
+
+export default Navbar
