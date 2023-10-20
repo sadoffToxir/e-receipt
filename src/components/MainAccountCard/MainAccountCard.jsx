9d8ffd7288d91a17a6fe149ba1ea5d475055f5a0
Tokhir Saidov <toxa.saidoff@gmail.com>
2023-10-20 20:01:45 +0200

    restoring olx components

---
+++ src/components/MainAccountCard/MainAccountCard.jsx
@@ -0,0 +1,15 @@
+import './MainAccountCard.css'
+
+const MainAccountCard = () => {
+  return (
+    <div className="card">
+      <p className="account-number">1234 1234 1234 1234</p>
+      <div>
+        <p className="name">Adilet Baimyrza Uulu</p>
+        <p className="amount">1500$</p>
+      </div>
+    </div>
+  )
+}
+
+export default MainAccountCard
